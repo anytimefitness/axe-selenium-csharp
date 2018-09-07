ce37108ba830f6ea9d88c2a267ca29c12db67149
Neil Joyer <nejoyer@users.noreply.github.com>
2018-09-07 15:19:46 -0500

    CORE-831 Add IncludeExcludeManagerTests

---
+++ Globant.Selenium.Axe/Globant.Selenium.Axe.Test/IncludeExcludeManagerTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Globant.Selenium.Axe.Test
+{
+    [TestClass]
+    public class IncludeExcludeManagerTests
+    {
+        [TestMethod]
+        public void GivenNoExtraSettings_WhenIGetContextJson_ThenTheResultShouldBeTheWholeDocument()
+        {
+            //arrange
+            var includeExcludeManager = new IncludeExcludeManager();
+
+            // act
+            var contextJson = includeExcludeManager.GetContextJson();
+
+            //Assert
+            contextJson.Should().BeEquivalentTo("document");
+        }
+
+        [TestMethod]
+        public void GivenThatOneSelectorIsIncluded_WhenIGetContextJson_ThenTheResultShouldIncludeThatSelector()
+        {
+            //arrange
+            var includeExcludeManager = new IncludeExcludeManager();
+            includeExcludeManager.IncludeSelector(new ByCssSelector("bar"));
+
+            // act
+            var contextJson = includeExcludeManager.GetContextJson();
+
+            //Assert
+            contextJson.Should().BeEquivalentTo("{\"include\":[[\"bar\"]]}");
+        }
+
+        [TestMethod]
+        public void GivenThatTwoSelectorsAreIncluded_WhenIGetContextJson_ThenTheResultShouldIncludeEachSelectorInTheirOwnArray()
+        {
+            //arrange
+            var includeExcludeManager = new IncludeExcludeManager();
+            includeExcludeManager.IncludeSelector(new ByCssSelector("foo"));
+            includeExcludeManager.IncludeSelector(new ByCssSelector("bar"));
+
+            // act
+            var contextJson = includeExcludeManager.GetContextJson();
+
+            //Assert
+            contextJson.Should().BeEquivalentTo("{\"include\":[[\"foo\"],[\"bar\"]]}");
+        }
+
+        [TestMethod]
+        public void GivenThatOneSelectorIsExcluded_WhenIGetContextJson_ThenTheResultShouldExcludeThatSelector()
+        {
+            //arrange
+            var includeExcludeManager = new IncludeExcludeManager();
+            includeExcludeManager.ExcludeSelector(new ByCssSelector("foobar"));
+
+            // act
+            var contextJson = includeExcludeManager.GetContextJson();
+
+            //Assert
+            contextJson.Should().BeEquivalentTo("{\"exclude\":[[\"foobar\"]]}");
+        }
+
+        [TestMethod]
+        public void GivenThatOneSelectorIsIncludedAndOneIsExcluded_WhenIGetContextJson_ThenTheResultShouldHaveBothIncludeAndExclude()
+        {
+            //arrange
+            var includeExcludeManager = new IncludeExcludeManager();
+            includeExcludeManager.IncludeSelector(new ByCssSelector("foo"));
+            includeExcludeManager.ExcludeSelector(new ByCssSelector("bar"));
+
+            // act
+            var contextJson = includeExcludeManager.GetContextJson();
+
+            //Assert
+            contextJson.Should().BeEquivalentTo("{\"include\":[[\"foo\"]],\"exclude\":[[\"bar\"]]}");
+        }
+
+        [TestMethod]
+        public void GivenThatOneSelectorIsIncludedFromAnIFram_WhenIGetContextJson_ThenTheResultShouldHaveTheSelectorAndIFrameSelectorInTheRightFormat()
+        {
+            //arrange
+            var includeExcludeManager = new IncludeExcludeManager();
+            includeExcludeManager.IncludeSelector(new ByCssSelector("foo").InIFrameIdentifiedByCssSelector("#iFrame"));
+
+            // act
+            var contextJson = includeExcludeManager.GetContextJson();
+
+            //Assert
+            contextJson.Should().BeEquivalentTo("{\"include\":[[\"foo\",\"#iFrame\"]]}");
+        }
+    }
+}
