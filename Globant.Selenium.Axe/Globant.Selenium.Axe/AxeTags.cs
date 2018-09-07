05d311b235eadcdc738bb33f822d09ab479f6171
Neil Joyer <nejoyer@users.noreply.github.com>
2018-09-07 15:20:24 -0500

    CORE-831 Add AxeTags enum so that you don't have typos or have to look up what the options are

---
+++ Globant.Selenium.Axe/Globant.Selenium.Axe/AxeTags.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globant.Selenium.Axe
+{
+    public enum AxeTags
+    {
+        [Description("wcag2a")]
+        Wcag2A,
+        [Description("wcag2aa")]
+        Wcag2AA,
+        [Description("section508")]
+        Section508,
+        [Description("best-practice")]
+        BestPractice,
+        [Description("experimental")]
+        Experimental
+    }
+}
