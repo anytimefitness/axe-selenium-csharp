f9205a3591d437a2bd7278e1fb0e803adba18bc5
Neil Joyer <nejoyer@users.noreply.github.com>
2018-09-07 15:19:23 -0500

    CORE-831 Update Include Exclude Manager to use CSSSelector builders

--- Globant.Selenium.Axe/Globant.Selenium.Axe/IncludeExcludeManager.cs
+++ Globant.Selenium.Axe/Globant.Selenium.Axe/IncludeExcludeManager.cs
@@ -2,7 +2,6 @@
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Globant.Selenium.Axe
 {
@@ -10,70 +9,51 @@ namespace Globant.Selenium.Axe
     /// Handle all initialization, serialization and validations for includeExclude aXe object.
     /// For more info check this: https://github.com/dequelabs/axe-core/blob/master/doc/API.md#include-exclude-object
     /// </summary>
+    [JsonObject]
     public class IncludeExcludeManager
     {
         private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
         {
-            Formatting = Formatting.None,
+            //Formatting = Formatting.Indented, //Useful for debugging, but will break unit tests.
             ContractResolver = new CamelCasePropertyNamesContractResolver(),
-            NullValueHandling = NullValueHandling.Ignore
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore
         };
 
-        private List<string[]> _includeList;
-        private List<string[]> _excludeList;
+        
+        private List<string[]> _includeList = new List<string[]>();
+        private List<string[]> _excludeList = new List<string[]>();
+
+        [JsonProperty]
+        private string[][] Include => _includeList.Count > 0 ? _includeList.ToArray() : null;
+
+        [JsonProperty]
+        private string[][] Exclude => _excludeList.Count > 0 ? _excludeList.ToArray() : null;
 
         /// <summary>
         /// Include the given selectors, i.e "#foo", "ul.bar .target", "div"
         /// </summary>
-        /// <param name="selectors">Selectors to include</param>
-        public void Include(params string[] selectors)
+        /// <param name="selector">Selectors to include</param>
+        public void IncludeSelector(ByCssSelector selector)
         {
-            ValidateParameters(selectors);
-            if (_includeList == null)
-                _includeList = new List<string[]>();
-            _includeList.Add(selectors);
+            if (selector == null)
+            {
+                throw new ArgumentNullException(nameof(selector));
+            }
+            _includeList.Add(selector.FormatForAxe());
         }
 
         /// <summary>
-        /// Include the given selectors, i.e "frame", "div.foo"
+        /// Exclude the given selectors, i.e "frame", "div.foo", "#foo"
         /// </summary>
-        /// <param name="selectors">Selectors to exclude</param>
-        public void Exclude(params string[] selectors)
+        /// <param name="selector">Selector to exclude</param>
+        public void ExcludeSelector(ByCssSelector selector)
         {
-            ValidateParameters(selectors);
-            if (_excludeList == null)
-                _excludeList = new List<string[]>();
-            _excludeList.Add(selectors);
-        }
-
-        /// <summary>
-        /// Indicate if we have more than one entry on include list or we have entries on exclude list
-        /// </summary>
-        /// <returns>True or False</returns>
-        private bool HasMoreThanOneSelectorsToIncludeOrSomeToExclude()
-        {
-            bool hasMoreThanOneSelectorsToInclude = _includeList != null && _includeList.Count > 1;
-            bool hasSelectorsToExclude = _excludeList != null && _excludeList.Count > 0;
-
-            return hasMoreThanOneSelectorsToInclude || hasSelectorsToExclude;
-        }
-
-        /// <summary>
-        /// Indicate we have one entry on the include list
-        /// </summary>
-        /// <returns>True or False</returns>
-        private bool HasOneItemToInclude() => _includeList != null && _includeList.Count == 1;
-
-        /// <summary>
-        /// Get first selector of the first entry on include list
-        /// </summary>
-        /// <returns></returns>
-        private string GetFirstItemToInclude()
-        {
-            if (_includeList == null || _includeList.Count == 0)
-                throw new InvalidOperationException("You must add at least one selector to include");
-
-            return _includeList.First().First();
+            if (selector == null)
+            {
+                throw new ArgumentNullException(nameof(selector));
+            }
+            _excludeList.Add(selector.FormatForAxe());
         }
 
         /// <summary>
@@ -82,12 +62,9 @@ namespace Globant.Selenium.Axe
         public string GetContextJson()
         {
             string command;
-            if (HasMoreThanOneSelectorsToIncludeOrSomeToExclude())
+            if (_includeList.Count > 0 || _excludeList.Count > 0)
             {
                 command = JsonConvert.SerializeObject(this, JsonSerializerSettings);
-            }else if (HasOneItemToInclude())
-            {
-                command = $"'{GetFirstItemToInclude().Replace("'", "")}'";
             }
             else
             {
@@ -95,15 +72,6 @@ namespace Globant.Selenium.Axe
             }
 
             return command;
-        }
-
-        private static void ValidateParameters(string[] selectors)
-        {
-            if (selectors == null)
-                throw new ArgumentNullException(nameof(selectors));
-
-            if (selectors.Any(string.IsNullOrEmpty))
-                throw new ArgumentException("There is some items null or empty", nameof(selectors));
         }
     }
 }
