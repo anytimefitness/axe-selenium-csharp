0532676ca2b80f8e73c4a1342cae23d7dff0abf8
Neil Joyer <nejoyer@users.noreply.github.com>
2018-09-07 15:16:00 -0500

    CORE-831 Update object model to expose errors that occur during the analyze step

---
+++ Globant.Selenium.Axe/Globant.Selenium.Axe/AxeDto/AxeResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globant.Selenium.Axe.AxeDto
+{
+    public class AxeResponse
+    {
+        public AxeError Error { get; set; }
+        public AxeResults Results { get; set; }
+    }
+}
