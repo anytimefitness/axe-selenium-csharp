3c7c92e612a086796489be07f71244d09fca8329
Neil Joyer <nejoyer@users.noreply.github.com>
2018-09-10 16:59:07 -0500

    CORE-831 Add ToString Override to make it easy to print results

--- Globant.Selenium.Axe/Globant.Selenium.Axe/AxeDto/AxeResultItem.cs
+++ Globant.Selenium.Axe/Globant.Selenium.Axe/AxeDto/AxeResultItem.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Globant.Selenium.Axe
 {
@@ -11,5 +13,26 @@ namespace Globant.Selenium.Axe
         public string Impact { get; set; }
         public string[] Tags { get; set; }
         public AxeResultNode[] Nodes { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"AxeResultItem:");
+            stringBuilder.AppendLine($"\tId: {Id}");
+            stringBuilder.AppendLine($"\tDescription: {Description}");
+            stringBuilder.AppendLine($"\tHelp: {Help}");
+            stringBuilder.AppendLine($"\tHelpUrl: {HelpUrl}");
+            stringBuilder.AppendLine($"\tImpact: {Impact}");
+            stringBuilder.AppendLine($"\tTags: {String.Join(",", Tags)}");
+            stringBuilder.AppendLine($"\tNodes:");
+            foreach (var axeResultNode in Nodes)
+            {
+                stringBuilder.AppendLine($"\t\t{axeResultNode.Html}");
+            }
+
+            stringBuilder.AppendLine();
+
+            return stringBuilder.ToString();
+        }
     }
 }
